162cad9945a8a80d366671c9f92add45a7dff9bf
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-14 04:16:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,9 +50,9 @@ const channel = sWlc[message.guild.id].channel
   if (message.content.startsWith(prefix + "setwelcome")) {
     if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
     let newChannel = message.content.split(' ').slice(1).join(" ")
-    if(!newChannel) return message.reply(`**رجاء كتابت اسم الروم**`)
+    if(!newChannel) return message.reply(`**$setwelcome رجاء كتابت اسم الروم**`)
     sWlc[message.guild.id].channel = newChannel
-    message.channel.send(`**${message.guild.name}'s channel has been changed to ${newChannel}**`);
+    message.channel.send(`**${message.guild.name}تم عمل الترحيب في روم ${newChannel}**`);
   }
 });
  
