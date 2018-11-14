7d9574d54e921058ed113351859b4e77533ca91c
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-14 05:10:54 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1238,21 +1238,7 @@ client.on('message',async message => {
     }
   });
 
-client.on('message', message => {
-  var prefix = "$"; /// غير البرفيكس
-  
-if (message.author.bot) return;
-if (!message.content.startsWith(prefix)) return;
-   
-let command = message.content.split(" ")[0];
-command = command.slice(prefix.length);
-   
-let args = message.content.split(" ").slice(1);
-   
-if (command === "say") {
-message.delete()
-  message.channel.sendMessage(args.join(" ")).catch(console.error);
-}
+
 
 	
 	
