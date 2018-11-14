c078053dcf865ce2215d6593c63cb11a8273bc40
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-14 05:13:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -137,6 +137,17 @@ client.on("guildMemberAdd", member => {
       }
       });
 
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : i1Suhaib`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`$help | $inv`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
+
 
 client.on('message', message => {
     if (message.author.bot) return;
