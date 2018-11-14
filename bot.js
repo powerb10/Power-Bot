3f3a302496d1b6d418ac76e0688aca4f160f1d18
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-14 05:09:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1255,16 +1255,7 @@ message.delete()
 }
 
 	
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : i1Suhaib`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`$help | $inv`,"http://twitch.tv/S-F")
-client.user.setStatus("online")
-});	
+	
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
