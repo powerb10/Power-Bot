6485d0be7d915f88ba5b7691acf59d58913742a3
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-17 13:59:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1817,35 +1817,7 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
     }
 });
 
-let bane = JSON.parse(fs.readFileSync("./bcer.json", "utf8"));
-let banse = new Set();
-client.on('guildBanAdd', function(guild) {
-  guild.fetchAuditLogs().then(logs => {
-    const ser = logs.entries.first().executor;
-    if(!bane[ser.id+guild.id]) bane[ser.id+guild.id] = {
-      bans: 2
-    }
-    let boner = bane[ser.id+guild.id]
-banse.add(ser.id)
-boner.bans = Math.floor(boner.bans+1)
 
-
-setTimeout(() => {
-  boner.bans = 2
-  banse.delete(ser.id)
-},8000)
-
-if(boner.bans > 2) {
-  let roles = guild.members.get(ser.id).roles.array()
-guild.members.get(ser.id).removeRoles(roles)
-}
-
-    })
-    fs.writeFile('./bcer.json', JSON.stringify(bane), (err) => {
-if (err) console.error(err);
-})
-
-})
 
 
 client.login(process.env.BOT_TOKEN);
