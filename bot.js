71c78e525ed6cd48caa3cdce8c01dcc648458dbc
powerb10 <45021880+powerb10@users.noreply.github.com>
2018-11-19 06:15:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1881,7 +1881,7 @@ client.on('guildBanAdd', function(guild) {
   guild.fetchAuditLogs().then(logs => {
     const ser = logs.entries.first().executor;
     if(!bane[ser.id+guild.id]) bane[ser.id+guild.id] = {
-      bans: 2
+      bans: 3
     }
     let boner = bane[ser.id+guild.id]
 banse.add(ser.id)
@@ -1889,11 +1889,11 @@ boner.bans = Math.floor(boner.bans+2)
 
 
 setTimeout(() => {
-  boner.bans = 2
+  boner.bans = 3
   banse.delete(ser.id)
 },8000)
 
-if(boner.bans > 2) {
+if(boner.bans > 3) {
   let roles = guild.members.get(ser.id).roles.array()
 guild.members.get(ser.id).removeRoles(roles)
 }
